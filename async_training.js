b4518429e7b0282196fe45ba641bee1fbd39f219
tmstani23 <tmstani23@gmail.com>
2018-11-14 18:59:25 -0600

    Update 2
    Added folder and information on async vs sync functions.  Created a couple basic test functions to test setTimeout and callbacks

--- async_training.js
+++ async_training.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+
+//Callbacks 
+    //Set Timeout async code
+    function first() {
+        setTimeout(() => console.log("first fired"),1000);  
+    }
+    function second() {
+        console.log("second fired")
+    }
+    //first function actually fires second because it's async and must wait one second to fire.
+    first();
+    second();
+    
+    
